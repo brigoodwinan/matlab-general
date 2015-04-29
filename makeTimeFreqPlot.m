5a65685c1aca6fb1cf800f32dd36e88c01d2ab32
Brian <brigoodwinan@gmail.com>
2015-04-28 19:40:07 -0500

    edited makeTimeFreqPlot.m to enable multiple plots in one figure.

--- makeTimeFreqPlot.m
+++ makeTimeFreqPlot.m
@@ -1,7 +1,10 @@
-function makeTimeFreqPlot(x,dt,s,scaling)
-% makeTimeFreqPlot(x,dt)
-% makeTimeFreqPlot(x,dt,s)
-% makeTimeFreqPlot(x,dt,s,scaling)
+function makeTimeFreqPlot(varargin)
+% makeTimeFreqPlot(x1,x2,...,x3,s,dt,scaling)
+% makeTimeFreqPlot(x1,x2,...,x3,s,dt)
+% makeTimeFreqPlot(x1,x2,...,x3,s,scaling)
+% makeTimeFreqPlot(x1,x2,...,x3,s)
+% makeTimeFreqPlot(x1,x2,...,x3,scaling)
+% makeTimeFreqPlot(...,'frequency')
 %
 % Brian Goodwin, 2015-03-05
 %
@@ -10,37 +13,88 @@ function makeTimeFreqPlot(x,dt,s,scaling)
 % signal on the bottom.
 %
 % INPUTS:
-% x: m-by-n matrix where x would be the output from x = stockwell(s). m is
-%     frequency axis and n is time axis.
+% x1..xN: m-by-n matrix where x would be the output from x = stockwell(s). m is
+%     frequency axis and n is time axis. All x's must have equal n.
 % s: 1D signal
 % dt: time step or time vector (n-by-1)
 % scaling: (optional) 1-by-2 array of the values to scale the color bar to.
-%     This is the last input of the imagesc function (e.g., [0,0.5], 
+%     This is the last input of the imagesc function (e.g., [0,0.5],
 %     default is[0,1].
+% string input: if string is 'frequency', then the output is
+%     time-frequency.
 
-nlen = size(x,2);
+nin = length(varargin);
+a = zeros(nin,1);
+b = zeros(nin,1);
+for k = 1:nin
+    [a,b] = size(varargin{k});
+end
 
-if nargin<4
+indTF = a>1 & b>1;
+indS = a==1 | b==1;
+indnS = find(indS);
+
+isFreq = false;
+
+for k = 1:sum(indS)
+    tmp = varargin{indnS(k)};
+    tmpn = length(tmp);
+    
+    if ischar(tmp)
+        if strcmpi(tmp,'frequency')
+            isFreq = true;
+        end
+        continue
+    end
+    
+    if tmpn==2
+        scaling = tmp;
+    end
+    
+    if tmpn==1 || (tmpn>2 && k>1)
+        dt = tmp;
+    end
+    
+    if tmpn>2 && k==1
+        s = tmp;
+    end
+end
+
+nlen = size(varargin{1},2);
+
+if ~exist('scaling','var')
     scaling = [0,1];
 end
-
-if length(dt)>1
-    t = dt;
-    dt = diff(t(1:2));
+if exist('dt','var')
+    if length(dt)>1
+        t = dt;
+    else
+        t = dt:dt:dt*nlen;
+    end
 else
-    t = dt:dt:dt*nlen;
+    t = 1:nlen;
 end
 
-if nargin<3
-    imagesc(t,linspace(0,1/2/dt,size(x,1)),abs(x),scaling);
-    axis xy
-    axis tight
-else
-    ax(1) = subplot(2,1,1);
+nplots = sum(indTF);
+axnum = 0;
+if exist('s','var')
+    nplots = nplots+1;
+    axnum = 1;
+    ax(1) = subplot(nplots,1,1);
     plot(t,s,'k');
-    axis tight
-    ax(2) = subplot(2,1,2);
-    imagesc(t,linspace(0,1/2/dt,size(x,1)),abs(x),scaling);
-    axis xy
-    linkaxes(ax,'x');
 end
+for k = 1:sum(indTF)
+    axnum = axnum+1;
+    ax(axnum) = subplot(nplots,1,axnum);
+    
+    if isFreq
+        x = linspace(0,1/2/dt,size(varargin{k},1));
+    else
+        x = 1:size(varargin{k},1);
+    end
+    
+    imagesc(t,x,varargin{k},scaling);
+    axis xy
+    axis tight
+end
+linkaxes(ax,'x');
