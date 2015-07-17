c06edd54cc37effbb9203b15ee80e4c6839379de
Brian <brigoodwinan@gmail.com>
2015-07-17 11:14:26 -0500

    ag

--- findCrossings.m
+++ findCrossings.m
@@ -8,14 +8,18 @@ function I = findCrossings(s,th,mode)
 % 'down' (falling below threshold), 'peaks' (find local maxima), or
 % 'valleys' (find local minima).
 %
+% Requires the signal processing toolbox for 'peaks' - uses findpeaks().
+%
 % INPUTS:
 % s: input signal (n-by-1 or n-by-m where m is the number of signals).
 % th: threshold value for crossings (scalar)
 % mode: a string that specifies the nature of the output:
 %         'up' - finds the points where the signal rises above threshold.
 %         'down' - finds the points where the signal falls below threshold.
+%   The following are not yet implemented {
 %         'peaks' - finds local maxima above the threshold.
 %         'valleys' - finds local minima below the threshold.
+%   }
 %
 % OUTPUTS:
 % I: logical array indicating the indices in signal "s" that pertain to the
@@ -27,8 +31,10 @@ if strcmpi(mode,'up')
 elseif strcmpi(mode,'down')
     I = s(2:end,:)<th & s(1:end-1,:)>th;
     I = cat(1,false(1,size(I,2)));
-elseif strcmpi(mode,'peaks')
-    up = findCrossings(s,th,'up');
-    down = findCrossings(s,th,'down');
-    
+% elseif strcmpi(mode,'peaks')
+%     up = findCrossings(s,th,'up');
+%     down = findCrossings(s,th,'down');
+
+else
+    error('Mode specified does not exist.')
 end
