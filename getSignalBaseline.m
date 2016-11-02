8ee633cd522ba290621299166402435a23ccdee6
brigoodwinan <brigoodwinan@gmail.com>
2016-11-02 09:36:07 -0500

    fixbaseline

--- getSignalBaseline.m
+++ getSignalBaseline.m
@@ -4,8 +4,8 @@ function out = getSignalBaseline(in)
 % Brian Goodwin 2016-08-29
 %
 % Outputs the baseline of signals in column vector form n-by-m where m is
-% the number of signals to provide the. The baseline output is a row array
-% (or a scalar value for n-by-1 column vector).
+% the number of signals for which the baseline is computed. The baseline
+% output is a row array (or a scalar value for n-by-1 column vector).
 %
 % To show partiality to the beginning of the signal, run the function
 % using:
@@ -23,5 +23,5 @@ out = zeros(1,m);
 for k = 1:m
     [n,edges] = histcounts(in(:,k));
     [~,I] = max(n);
-    out(k) = mean(in(in(:,k)>edges(I) & in(:,k)<edges(I+1),k));
+    out(k) = mean(in(in(:,k)>=edges(I) & in(:,k)<=edges(I+1),k));
 end
